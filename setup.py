e3560862601ece283589fe47b4322fb03712a2a9
jmoore <josh@glencoesoftware.com>
2014-04-24 16:51:02 +0200

    Initial migration from omego with minimal changes
    Both omego and scc are based on a copy of the same
    code which is loosely copied from OMERO. This finally
    moves that code out to a re-usable component in prep
    for use by corgi as well.

---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2013 University of Dundee & Open Microscopy Environment
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+Yet-Another CLI Framework
+"""
+
+from setuptools import setup
+from yaclifw.version import get_git_version
+
+
+VERSION = get_git_version()
+ZIP_SAFE = False
+
+
+LONG_DESCRIPTION = open("README.rst", "r").read()
+
+CLASSIFIERS = ["Development Status :: 4 - Beta",
+               "Environment :: Console",
+               "Intended Audience :: Developers",
+               "Intended Audience :: System Administrators",
+               "License :: OSI Approved :: GNU General Public License v2"
+               " (GPLv2)",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Database :: Database Engines/Servers",
+               "Topic :: System :: Software Distribution",
+               "Topic :: System :: Systems Administration",
+               "Topic :: Utilities"]
+
+setup(name='yaclifw',
+
+      # Simple strings
+      author='The Open Microscopy Team',
+      author_email='ome-devel@lists.openmicroscopy.org.uk',
+      description='Framework for building CLI tools',
+      license='GPLv2',
+      url='https://github.com/ome/yaclifw',
+
+      # More complex variables
+      packages=['yaclifw'],
+      include_package_data=True,
+      install_requires=[],  # Skipping argparse for Python 2.7 and greater.
+      entry_points={'console_scripts': ['yaclifw = yaclifw.main:entry_point']},
+      zip_safe=ZIP_SAFE,
+
+      # Using global variables
+      long_description=LONG_DESCRIPTION,
+      classifiers=CLASSIFIERS,
+      version=VERSION,
+      )
