d4099cb6481da39a8396b236ba956d778bc43b6c
Simon Li <spli@dundee.ac.uk>
2014-06-25 00:14:22 +0100

    Add unchanged argparseconfig and test from manics/omego@134ee962577b8ebe3747e597a5ca16beedde7c57

---
+++ test/unit/test_argparseconfig.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2014 University of Dundee & Open Microscopy Environment
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import pytest
+
+import argparse
+from ConfigParser import SafeConfigParser, NoSectionError
+from omego import argparseconfig
+
+
+class TestArgparseConfigParser(object):
+
+    def setup_method(self, method):
+        cp = SafeConfigParser()
+
+        cp.add_section('main')
+        cp.set('main', 'int', '1')
+
+        cp.add_section('subparser1')
+        cp.set('subparser1', 'string', 'a string')
+
+        cp.add_section('subgroup1')
+        cp.set('subgroup1', 'float', '2.3')
+
+        self.cp = cp
+
+    def assert_args_equal(self, refdict, args):
+        """
+        Asserts that the list of public attributes of an object and the values
+        of those attributes are equal to refdict
+        """
+        assert refdict == args.__dict__
+
+    def assert_args_contains(self, refdict, obj):
+        """
+        Asserts that the list of public attributes of an object and the values
+        of those attributes includes those in refdict
+        """
+        for k, v in refdict.iteritems():
+            assert hasattr(obj, k)
+            assert getattr(obj, k) == v
+
+    @pytest.mark.parametrize('mode', ['noargs', 'args'])
+    @pytest.mark.parametrize('add_to_self', [True, False])
+    def test_add_and_parse_config_files(self, tmpdir, mode, add_to_self):
+        cfg1txt = [
+            '[main]',
+            'a = 1',
+            '[subgroup]',
+            'b = 2',
+        ]
+        cfg2txt = [
+            '[subgroup]',
+            'b = 3'
+        ]
+
+        cfg1 = tmpdir.join('f1.cfg')
+        cfg1.write('\n'.join(cfg1txt))
+        cfg2 = tmpdir.join('f2.cfg')
+        cfg2.write('\n'.join(cfg2txt))
+
+        argv1 = ['-c', str(cfg1), '--conffile', str(cfg2)]
+        if mode == 'noargs':
+            argv2 = []
+            expected2 = {'a': 1, 'b': None}
+        else:
+            argv2 = ['-a', '10', '-b', '20']
+            expected2 = {'a': 10, 'b': 20}
+        if add_to_self:
+            expected2['conffile'] = []
+        argv = argv1 + argv2
+
+        parser = argparseconfig.ArgparseConfigParser(add_help=False)
+        parsed, remaining, config, cfgparser = \
+            parser.add_and_parse_config_files(
+                '-c', '--conffile', args=argv, config_section='main',
+                add_to_self=add_to_self)
+
+        assert parser.config_parser == config
+        self.assert_args_equal({'conffile': [str(cfg1), str(cfg2)]}, parsed)
+        assert remaining == argv2
+        assert config.sections() == ['main', 'subgroup']
+        assert dict(config.items('main')) == {'a': '1'}
+        assert dict(config.items('subgroup')) == {'b': '3'}
+
+        parser.add_argument('-a', type=int)
+        parser.add_argument('-b', type=int)
+        parsed2 = parser.parse_args(remaining)
+
+        self.assert_args_equal(expected2, parsed2)
+
+    @pytest.mark.parametrize('config', ['match', 'nomatch', 'none'])
+    @pytest.mark.parametrize('default', [None, 1])
+    def test_set_action_default_from_config(self, config, default):
+        action = argparse.Action('--test', 'test', default=default)
+
+        if config == 'match':
+            cd = {'test': 2}
+            expected = {'default': 2}
+        elif config == 'nomatch':
+            cd = {'other': 3}
+            expected = {'default': default}
+        else:
+            cd = None
+            expected = {'default': default}
+        argparseconfig.set_action_default_from_config(action, cd)
+
+        self.assert_args_contains(expected, action)
+
+    @pytest.mark.parametrize('config', ['dict', 'parser'])
+    @pytest.mark.parametrize('mode', ['noargs', 'args'])
+    def test_subgroup(self, config, mode):
+        if config == 'dict':
+            acpargs = {'config_dict': dict(self.cp.items('main'))}
+            subgargs = {'config_dict': dict(self.cp.items('subgroup1'))}
+        else:
+            acpargs = {'config_parser': self.cp, 'config_section': 'main'}
+            subgargs = {'config_section': 'subgroup1'}
+
+        parser = argparseconfig.ArgparseConfigParser(**acpargs)
+        parser.add_argument('--int', type=int)
+
+        subgroup1 = parser.add_argument_group('subgroup1', **subgargs)
+        subgroup1.add_argument('--float', type=float)
+
+        if mode == 'noargs':
+            argv = []
+            expected = {'int': 1, 'float': 2.3}
+        elif mode == 'args':
+            argv = ['--int', '2', '--float', '4.5']
+            expected = {'int': 2, 'float': 4.5}
+
+        args = parser.parse_args(argv)
+        self.assert_args_equal(expected, args)
+
+    @pytest.mark.parametrize('config', ['dict', 'parser'])
+    @pytest.mark.parametrize('mode', ['noargs', 'nosubargs', 'args'])
+    def test_subparser(self, config, mode):
+        if config == 'dict':
+            acpargs = {'config_dict': dict(self.cp.items('main'))}
+            subpargs = {'config_dict': dict(self.cp.items('subparser1'))}
+            subgargs = {'config_dict': dict(self.cp.items('subgroup1'))}
+        else:
+            acpargs = {'config_parser': self.cp, 'config_section': 'main'}
+            subpargs = {'config_section': 'subparser1'}
+            subgargs = {'config_section': 'subgroup1'}
+
+        parser = argparseconfig.ArgparseConfigParser(**acpargs)
+        parser.add_argument('--int', type=int)
+
+        subparsers = parser.add_subparsers()
+        subparser1 = subparsers.add_parser('subparser1', **subpargs)
+        subparser1.add_argument('--string')
+
+        subgroup1 = subparser1.add_argument_group('subgroup1', **subgargs)
+        subgroup1.add_argument('--float', type=float)
+
+        if mode == 'noargs':
+            argv = []
+            # argparse calls system.exit on failure
+            expected = SystemExit
+        elif mode == 'nosubargs':
+            argv = ['--int', '2', 'subparser1']
+            expected = {'int': 2, 'string': 'a string', 'float': 2.3}
+        elif mode == 'args':
+            argv = ['--int', '2', 'subparser1', '--string', 'zzz',
+                    '--float', '4.5']
+            expected = {'int': 2, 'string': 'zzz', 'float': 4.5}
+
+        if expected == SystemExit:
+            with pytest.raises(expected):
+                args = parser.parse_args(argv)
+        else:
+            args = parser.parse_args(argv)
+            self.assert_args_equal(expected, args)
+
+    @pytest.mark.parametrize('ignore_missing', [True, False])
+    @pytest.mark.parametrize('missing', [True, False])
+    def test_get_set_config(self, ignore_missing, missing):
+        acpargs = {'config_parser': self.cp, 'config_section': 'main'}
+        if not ignore_missing:
+            acpargs['ignore_missing'] = False
+        if missing:
+            acpargs['config_section'] = 'missing'
+
+        if ignore_missing or not missing:
+            parser = argparseconfig.ArgparseConfigParser(**acpargs)
+            if missing:
+                assert parser.config_dict == {}
+            else:
+                assert parser.config_dict == {'int': '1'}
+        else:
+            if missing:
+                with pytest.raises(NoSectionError):
+                    parser = argparseconfig.ArgparseConfigParser(**acpargs)
+            else:
+                parser = argparseconfig.ArgparseConfigParser(**acpargs)
+                assert parser.config_dict == {'int': '1'}
---
+++ yaclifw/argparseconfig.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2014 University of Dundee & Open Microscopy Environment
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+A combination of command-line argument parsing using argparse and
+configuration file options using ConfigParser
+"""
+
+import argparse
+import ConfigParser
+
+
+def set_action_default_from_config(action, config_dict):
+    """
+    If config_dict contains a matching key matching action.dest then use this
+    to set the default value of the argparse action object
+    """
+    if config_dict:
+        try:
+            assert action.dest
+            default = config_dict[action.dest]
+            if action.type:
+                default = action.type(default)
+            print 'Default %s: %s -> %s' % (
+                action.dest, action.default, default)
+            action.default = default
+        except KeyError:
+            pass
+
+
+class ArgumentGroupParser(object):
+    """
+    Overrides the add_argument method of the object returned by
+    ArgumentParser.add_argument_group()
+    """
+
+    def __init__(self, group, config_dict):
+        self.group = group
+        self.config_dict = config_dict
+
+    def add_argument(self, *args, **kwargs):
+        """
+        Overrides add_argument so that the string values from a configuration
+        file can be converted to the required type if specified.
+        """
+        has_default = 'default' in kwargs
+        print 'ArgumentGroupParser.add_argument', args, kwargs, has_default
+        action = self.group.add_argument(*args, **kwargs)
+        set_action_default_from_config(action, self.config_dict)
+        return action
+
+    def __getattr__(self, name):
+        return getattr(self.group, name)
+
+
+class ArgparseConfigParser(argparse.ArgumentParser):
+    """
+    An ArgumentParser that uses ConfigParser values as defaults, inspired by
+    http://blog.vwelch.com/2011/04/combining-configparser-and-argparse.html
+
+    Instead of using ArgumentParser.set_defaults() this overrides
+    ArgumentParser.add_argument so that the `type` argument can be used to
+    convert strings from the ConfigParser into the specified type
+
+    Optional keyword arguments:
+      Only one of config_dict or config_parser can be provided
+      config_dict, dict: A dictionary of arguments and default values, for
+        example dict(ConfigParser.items())
+      config_parser, ConfigParser: A ConfigParser object, if specified then
+        config_section should also be given
+      config_section, str: The ConfigParser section name, if not specified
+        then no config values will be used for this parser
+      ignore_missing, bool: If True (default) missing configuration
+        sections will be ignored. If configuration files are optional this
+        must be True.
+
+
+    Overridden methods
+
+    add_argument()
+      Overrides add_argument so that the string values from a configuration
+      file can be converted to the required type if necessary.
+
+    add_argument_group(..., config_dict=dict, config_section=str)
+      Takes an optional set of config file values, one of the following can be
+      specified:
+        config_dict, dict: A dictionary of arguments and default values
+        config_section, str: The ConfigParser section name, a config_parser
+          must have been provided when constructing the ArgparseConfigParser
+
+    add_subparsers()
+      Returns a modified subparsers object that includes a link to the parent
+      config values. This object includes a method addparser().
+
+      addparser()
+        Takes the same optional config arguments as ArgparseConfigParser,
+        except that config_parser from the parent parser will be automatically
+        set if not otherwise provided, so just config_section can be
+        specified.
+
+
+    Additional methods
+
+    add_and_parse_config_files(*optionargs,
+        args=None, config_section=None, add_help=True, add_to_self=False)
+      Looks for config-files, and attempts to parse them. If multiple
+      config-file arguments are passed they will be merged with values from
+      later files overridding earlier ones.
+
+      The configurations will be added to this object. If add_to_self is True
+      then the argument parser will be added to this object, otherwise they
+      will be added to a separate parser.
+
+      Note this will call parse_known_args() to find any config files in the
+      command line arguments. This means if help was requested the parser will
+      exit before all other arguments have been added. To prevent this pass
+      add_help=False to ArgparseConfigParser, and add it after the initial
+      parse to find the config files has been completed.
+
+      optionargs: short and/or long option names for indicating config file
+        arguments
+      args: Optional, array of arguments to be parsed
+      config_section: The name of the section in the config-file to use
+      add_help: Whether to add a help argument
+      return: (parsed_args, remaining_args, config)
+        parsed_args: A Namespace object containing the parsed arguments
+        remaining_args: Array of args which weren't parsed
+        config: The ConfigParser object with all config files combined
+      e.g. add_and_parse_config_files(
+             '-c', '--conffile', ..., config_section='section')
+
+    set_config(config_parser, config_section=None)
+      Sets or changes the config_parser, see above
+
+
+    Examples:
+
+    config = ConfigParser.SafeConfigParser()
+    # config.cfg must contain sections 'main', 'subparser' and 'subgroup'
+    config.read('config.cfg')
+    config_main = config.items('main')
+    config_subparser = config.items('subparser')
+    config_group = config.items('subgroup')
+
+    parser = ArgparseConfigParser(config_dict=dict(config_main))
+    group = parser.add_argument_group('title', config_dict=dict(config_group))
+
+    parser = ArgparseConfigParser(config_parser=config, config_section='main')
+    subparsers = parser.add_subparsers(title='Subparsers')
+    subparser = subparsers.add_parser(
+        'subparser', help='Subparser help', config_section='subparser')
+    group = subparser.add_argument_group('title', config_section='subgroup')
+
+    argv = '-c f1.cfg -c f2.cfg -a 1 -b 1'.split()
+    parser = ArgparseConfigParser(add_help=False)
+    parsed, remaining, config = parser.add_and_parse_config_files(
+        '-c', '--conffile', args=argv, config_section='section', add_help=True)
+    # c1.cfg and c2.cfg will be read and the required section used to set the
+    # defaults for subsequent add_argument() calls
+    parser.add_argument('-a', help='Help for a')
+    args = parser.parse_args(remaining)
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.config_dict = kwargs.pop('config_dict', None)
+        self.config_parser = kwargs.pop('config_parser', None)
+        config_section = kwargs.pop('config_section', None)
+        self.ignore_missing = bool(kwargs.pop('ignore_missing', True))
+
+        if self.config_dict and self.config_parser:
+            raise Exception('Invalid combination of arguments')
+        if self.config_parser:
+            self.config_dict = self.get_config_section(config_section)
+
+        super(ArgparseConfigParser, self).__init__(*args, **kwargs)
+        self.subparsers = None
+
+    def add_and_parse_config_files(self, *optionargs, **kwargs):
+        # Must disable help, otherwise parse_args will exit prematurely before
+        # the subparsers have been added. Unfortunately this means the config
+        # files will always be read even if help is specified
+
+        args = kwargs.pop('args', None)
+        config_section = kwargs.pop('config_section', None)
+        add_help = kwargs.pop('add_help', True)
+        add_to_self = kwargs.pop('add_to_self', False)
+
+        if add_to_self:
+            cfgparser = self
+        else:
+            cfgparser = ArgparseConfigParser(add_help=False)
+        cfgparser.add_argument(
+            *optionargs, action='append', default=[],
+            help='Configuration file (can be repeated)', metavar='FILE.cfg')
+
+        parsed_args, remaining_args = cfgparser.parse_known_args(args)
+        config = ConfigParser.SafeConfigParser()
+        # TODO: Read defaults in from a file
+        # config.readfp(open('defaults.cfg'))
+        if parsed_args.conffile:
+            files_read = config.read(parsed_args.conffile)
+        else:
+            files_read = []
+        unread = set(parsed_args.conffile) - set(files_read)
+        if unread:
+            raise Exception(
+                'Failed to read configuration file(s): %s' % ' '.join(unread))
+
+        if add_help:
+            cfgparser.add_argument(
+                '-h', '--help', action='help', default=argparse.SUPPRESS,
+                help='show this help message and exit')
+
+        self.set_config(config, config_section)
+        return parsed_args, remaining_args, config, cfgparser
+
+    def add_argument(self, *args, **kwargs):
+        has_default = 'default' in kwargs
+        print 'ArgparseConfigParser.add_argument', args, kwargs, has_default
+        action = super(ArgparseConfigParser, self).add_argument(
+            *args, **kwargs)
+        set_action_default_from_config(action, self.config_dict)
+        return action
+
+    def add_argument_group(self, *args, **kwargs):
+        print 'ArgparseConfigParser.add_argument_group', args, kwargs
+        group_config_section = kwargs.pop('config_section', None)
+        group_config_dict = kwargs.pop('config_dict', None)
+
+        if group_config_section and group_config_dict is not None:
+            raise Exception('Invalid combination of arguments')
+
+        if group_config_section is not None:
+            if not self.config_parser:
+                raise Exception('No ConfigParser was provided')
+            group_config_dict = self.get_config_section(group_config_section)
+        elif group_config_dict is None:
+            group_config_dict = self.config_dict
+        # Allow {} to indicate no config values
+
+        group = super(ArgparseConfigParser, self).add_argument_group(
+            *args, **kwargs)
+        return ArgumentGroupParser(group, group_config_dict)
+
+    def add_subparsers(self, *args, **kwargs):
+        class SubParsers:
+            def __init__(self, parent):
+                self.parent = parent
+
+            def add_parser(self, *args, **kwargs):
+                kwargs.setdefault('config_parser', self.parent.config_parser)
+                return self.parent.subparsers.add_parser(*args, **kwargs)
+
+        self.subparsers = super(ArgparseConfigParser, self).add_subparsers(
+            *args, **kwargs)
+        return SubParsers(self)
+
+    def set_config(self, config_parser, config_section=None):
+        self.config_parser = config_parser
+        self.config_dict = self.get_config_section(config_section)
+
+    def get_config_section(self, section):
+        if section is None:
+            return {}
+            # raise Exception('No ConfigParser section name provided')
+        try:
+            return dict(self.config_parser.items(section))
+        except ConfigParser.NoSectionError:
+            if self.ignore_missing:
+                return {}
+            raise
